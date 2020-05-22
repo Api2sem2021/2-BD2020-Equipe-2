6d38145ad8ef993dcf822ea8d05295ab9c998210
Igor Carvalho <igcraval@gmail.com>
2020-05-22 19:52:18 +0000

    Update do script do banco, criando tableas e colunas mas com problemas na Foreign Key

--- Projeto/Pineapple.sql
+++ Projeto/Pineapple.sql
@@ -1,46 +1,64 @@
 create database Pineapple;
 
+use pineapple;
+
 create table Projeto (
-	nome_projeto varchar(50) primary key not null,
-    data_inicio datetime primary key not null,
-    data_termino datetime default null
+	nome_projeto varchar(50) not null,
+    data_inicio datetime not null,
+    data_termino datetime default null,
+    primary key(nome_projeto, data_inicio)
 );
 
 create table Tarefa (
-	nome_tarefa varchar(50) primary key not null,
-    data_inicio datetime primary key not null,
-    data_termino datetime default null
+	nome_tarefa varchar(50)  not null,
+    data_inicio datetime not null,
+    data_termino datetime default null,
+    primary key (nome_tarefa, data_inicio)
 );
 
-create table Recursos (
-	cpf varchar(12) primary key not null,
-	nome_recurso varchar(100) primary key,
-    email varchar(50)
+create table Recurso (
+	cpf varchar(12)  not null,
+	nome_recurso varchar(100),
+    email varchar(50),
+    primary key (cpf, nome_recurso)
 );
 
 create table Rel_Tarefa_Recurso (
-	rel_Tarefa varchar(12) primary key,
-    rel_Recruso varchar(100) primary key,
-    constraint foreign key (rel_recurso)
-	references Recurso (cpf, nome_recurso) on delete restrict on update cascade,
-    constraint foreign key (rel_tarefa)
-	references Tarefa (nome_tarefa, data_Inicio) on delete restrict on update cascade
+	rel_tarefa varchar(12),
+    rel_recurso varchar(100),
+    primary key (rel_tarefa, rel_recurso)
 );
 
 create table Rel_Tarefa_Projeto (
-	rel_Tarefa varchar(50) primary key,
-    rel_Projeto varchar(50) primary key,
-    constraint foreign key (Rel_tarefa)
-	references Tarefa (nome_tarefa, data_Inicio) on delete restrict on update cascade,
-    constraint foreign key (rel_projeto) 
-	references Projeto (nome_projeto, data_inicio) on delete restrict on update cascade
+	rel_tarefa varchar(50),
+    rel_projeto varchar(50),
+    primary key (rel_tarefa, rel_projeto)
 );
 
 create table Rel_Projeto_Recurso (
-	rel_Projeto varchar(50),
-    rel_Recurso varchar(100),
-    constraint foreign key (rel_projeto) 
-	references Projeto (nome_projeto, data_inicio) on delete restrict on update cascade,
-     constraint foreign key (rel_recurso)
-	references Recurso (cpf, nome_recurso) on delete restrict on update cascade
+	rel_projeto varchar(50),
+    rel_recurso varchar(100),
+    primary key (rel_projeto, rel_recurso)
 );
+
+/* Problemas com a foreign key */
+
+alter table rel_tarefa_recurso 
+	add constraint foreign key (rel_recurso)
+	references Recurso (cpf, nome_recurso) on delete restrict on update cascade, 
+    add constraint foreign key (rel_tarefa)
+	references Tarefa (nome_tarefa, data_Inicio) on delete restrict on update cascade;
+    
+alter table rel_tarefa_projeto
+	add constraint foreign key (Rel_tarefa)
+	references Tarefa (nome_tarefa, data_Inicio) on delete restrict on update cascade,
+    add constraint foreign key (rel_projeto) 
+	references Projeto (nome_projeto, data_inicio) on delete restrict on update cascade;
+    
+alter table re_projeto_recurso
+	add constraint foreign key (rel_projeto) 
+	references Projeto (nome_projeto, data_inicio) on delete restrict on update cascade,
+    add constraint foreign key (rel_recurso)
+	references Recurso (cpf, nome_recurso) on delete restrict on update cascade;
+    
+    
